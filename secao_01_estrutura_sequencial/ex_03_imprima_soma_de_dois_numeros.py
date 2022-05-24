7aa9c19031c4d7edc2edf872e071d4e0e8f4c590
Evandro Costa <evandropcs@hotmail.com>
2022-05-24 14:49:04 -0300

    ex 03 - secao 01

--- secao_01_estrutura_sequencial/ex_03_imprima_soma_de_dois_numeros.py
+++ secao_01_estrutura_sequencial/ex_03_imprima_soma_de_dois_numeros.py
@@ -15,3 +15,7 @@ Faça um Programa que peça dois números inteiros e imprima a soma.
 
 def imprima_a_soma_de_dois_numeros():
     """Escreva aqui em baixo a sua solução"""
+    n1=int(input('Número 1: '))
+    n2=int(input('Número 2: '))
+    soma=int(n1+n2)
+    print(f'A soma dos dois números informados é {soma}')
