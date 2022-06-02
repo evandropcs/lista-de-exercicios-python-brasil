d7f12510d0f0d1c8ab468f6ad090e7c7c6e27a00
Evandro Costa <evandropcs@hotmail.com>
2022-06-02 10:21:59 -0300

    atualizar

--- secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
+++ secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
@@ -6,7 +6,7 @@ Faça um Programa que converta metros para centímetros.
 
     >>> from secao_01_estrutura_sequencial import ex_05_metros_para_centimetros
     >>> ex_05_metros_para_centimetros.input = lambda k: '1'
-    >>> ex_05_metros_para_centimetros.converter_metros_para_centimetros()
+    >>> ex_05_metros_para_centimetros.conveos_para_centimetros()
     Transformando para centímetros dá 100.0 cm
     >>> ex_05_metros_para_centimetros.input = lambda k: '3.621'
     >>> ex_05_metros_para_centimetros.converter_metros_para_centimetros()
